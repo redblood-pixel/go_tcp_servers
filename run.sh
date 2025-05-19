e56599433595826adc72e8b00ea6a35e3569901e
redblood-pixel <danka2202@yandex.ru>
2025-05-19 15:36:52 +0300

    hot fixes

--- run.sh
+++ run.sh
@@ -20,7 +20,7 @@ sudo cp log_server /usr/loca/bin/log_server
 cd ..
 sudo cp server1.service /etc/systemd/system/server1.service
 sudo cp server2.service /etc/systemd/system/server2.service
-sudo cp log_server.service /etc/systemd/system/server2.service
+sudo cp log_server.service /etc/systemd/system/log_server.service
 
 
 sudo systemctl daemon-reload
